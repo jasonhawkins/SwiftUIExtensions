c1b55a515dc5ca434900eb81cacdc42bd5e3951a
Jason Hawkins <hellojason@me.com>
2019-06-07 15:09:38 -0400

    Add ContentSizeCategory

--- Sources/SwiftUIExtensions/SwiftUIExtensions.swift
+++ Sources/SwiftUIExtensions/SwiftUIExtensions.swift
@@ -1,3 +1,29 @@
-struct SwiftUIExtensions {
-    var text = "Hello, World!"
+//
+//  SwiftUIExtensions.swift
+//  SwiftUIExtensions
+//
+//  Created by Jason Hawkins on 6/7/19.
+//  Copyright © 2019 Jason Hawkins. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+extension ContentSizeCategory {
+    
+    /// Returns an array of every available size category.
+    public static var common: [ContentSizeCategory] = [
+        .extraSmall,
+        .small,
+        .medium,
+        .large,
+        .extraLarge,
+        .extraExtraLarge,
+        .extraExtraExtraLarge,
+        .accessibilityMedium,
+        .accessibilityLarge,
+        .accessibilityExtraLarge,
+        .accessibilityExtraExtraLarge,
+        .accessibilityExtraExtraExtraLarge
+    ]
 }
