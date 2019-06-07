64be09e1431f49d5c7bffe5c6be472a330654d1e
Jason Hawkins <hellojason@me.com>
2019-06-07 15:19:06 -0400

    Add unit test for ContentSizeCategory extension

--- Tests/SwiftUIExtensionsTests/SwiftUIExtensionsTests.swift
+++ Tests/SwiftUIExtensionsTests/SwiftUIExtensionsTests.swift
@@ -1,15 +1,27 @@
 import XCTest
+import SwiftUI
 @testable import SwiftUIExtensions
 
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 final class SwiftUIExtensionsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftUIExtensions().text, "Hello, World!")
+    
+    // MARK: - ContentSizeCategory
+    func testCommon_ReturnsAllSizeCategoryCases() {
+        let cases = ContentSizeCategory.common
+        
+        XCTAssertTrue(cases.count == 12, "Unexpected number of size categories.")
+        
+        XCTAssertEqual(cases[0], .extraSmall)
+        XCTAssertEqual(cases[1], .small)
+        XCTAssertEqual(cases[2], .medium)
+        XCTAssertEqual(cases[3], .large)
+        XCTAssertEqual(cases[4], .extraLarge)
+        XCTAssertEqual(cases[5], .extraExtraLarge)
+        XCTAssertEqual(cases[6], .extraExtraExtraLarge)
+        XCTAssertEqual(cases[7], .accessibilityMedium)
+        XCTAssertEqual(cases[8], .accessibilityLarge)
+        XCTAssertEqual(cases[9], .accessibilityExtraLarge)
+        XCTAssertEqual(cases[10], .accessibilityExtraExtraLarge)
+        XCTAssertEqual(cases[11], .accessibilityExtraExtraExtraLarge)
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
